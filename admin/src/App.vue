a15170a8ac4122ea5dde6ce69cf67a506e617d33
sangaizhi@163.com <sangaizhi@163.com>
2017-12-26 14:33:48 +0800

    admin 重构

---
+++ admin/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="app">
+		<router-view class='view'></router-view>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'app',
+		data() {
+			return {}
+		}
+	}
+</script>
+
+<style lang="scss">
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: #2c3e50;
+		width: 100%;
+		height: 100%;
+	}
+</style>
